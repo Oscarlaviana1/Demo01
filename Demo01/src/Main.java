0d2c6ffee30194be7b130dcb62ad88b32a655304
Oscar <uo258238@uniovi.es>
2021-09-30 09:18:02 +0200

    Demo01 now sums 3 + 8

--- Demo01/src/Main.java
+++ Demo01/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 8;
+		int c = a + b;
+		
 		System.out.println("Hello World!");
+		
+		System.out.println("if you add " + a + " to " + b +
+				" you'll get " + c);
 
 	}
 
